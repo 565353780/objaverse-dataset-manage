8e824bd3e4e784f3b0ff03f1f0c1894fab231bd3
chLi <565353780@qq.com>
2024-12-07 19:58:44 +0800

    add config files from objaverse_filter

--- download_kiuiv2.py
+++ download_kiuiv2.py
@@ -3,7 +3,7 @@ from objaverse_dataset_manage.Module.kiuiv2_downloader import Downloader
 
 if __name__ == "__main__":
     dataset_root_folder_path = '/home/chli/chLi/Dataset/'
-    csv_file_path = '../objaverse_filter/kiuisobj_v1_merged_80K.csv'
+    csv_file_path = './objaverse_dataset_manage/Config/kiuisobj_v1_merged_80K.csv'
     num_threads = os.cpu_count()
 
     downloader = Downloader(dataset_root_folder_path)
