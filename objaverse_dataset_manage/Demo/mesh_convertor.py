63823117eef25658300e1e9209ba15c15f3a2c05
chLi <565353780@qq.com>
2024-12-15 21:30:15 +0800

    add root dataset folder search

--- objaverse_dataset_manage/Demo/mesh_convertor.py
+++ objaverse_dataset_manage/Demo/mesh_convertor.py
@@ -2,7 +2,20 @@ import os
 from objaverse_dataset_manage.Module.mesh_convertor import MeshConvertor
 
 def demo():
-    dataset_root_folder_path = os.environ['HOME'] + '/chLi/Dataset/'
+    root_list = [
+        '/mnt/data/jintian/chLi/Dataset/',
+        os.environ['HOME'] + '/chLi/Dataset/',
+    ]
+    dataset_root_folder_path = None
+    for root in root_list:
+        if os.path.exists(root):
+            dataset_root_folder_path = root
+            break
+    if dataset_root_folder_path is None:
+        print('[ERROR][mesh_convertor::demo]')
+        print('\t dataset not found!')
+        return False
+
     dataset_name = 'Objaverse_82K'
     force_start = False
     worker_num = 1
