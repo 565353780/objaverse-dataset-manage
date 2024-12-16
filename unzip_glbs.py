f63e3a3c39d1d3818d85e70f37a575af5e210563
chLi <565353780@qq.com>
2024-12-16 20:30:01 +0800

    add stop convert mesh bash file, update unzip glbs script

--- unzip_glbs.py
+++ unzip_glbs.py
@@ -19,7 +19,7 @@ def unzipGlbs(glbs_zip_folder_path: str,
 
         zip_id = int(glbs_zip_filename[4:7])
 
-        if zip_id >= 123:
+        if zip_id >= 160:
             continue
 
         glbs_zip_file_path = glbs_zip_folder_path + glbs_zip_filename
@@ -33,7 +33,22 @@ def unzipGlbs(glbs_zip_folder_path: str,
     return True
 
 if __name__ == "__main__":
-    glbs_zip_folder_path = os.environ['HOME'] + '/Downloads/'
-    objaverse_glbs_folder_path = os.environ['HOME'] + '/chLi/Dataset/Objaverse_82K/glbs/'
+    root_list = [
+        '/mnt/data/jintian/chLi/Dataset/',
+        os.environ['HOME'] + '/chLi/Dataset/',
+    ]
+
+    root_folder_path = None
+    for root in root_list:
+        if os.path.exists(root):
+            root_folder_path = root
+            break
+    if root_folder_path is None:
+        print('[ERROR][unzip_glbs::__main__]')
+        print('\t dataset not found!')
+        exit()
+
+    glbs_zip_folder_path = os.environ['HOME'] + '/Downloads/finished/'
+    objaverse_glbs_folder_path = root_folder_path + 'Objaverse_82K/glbs/'
 
     unzipGlbs(glbs_zip_folder_path, objaverse_glbs_folder_path)
