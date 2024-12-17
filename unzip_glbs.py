e35d3609fae2c488681e9efafe65b77cb2452947
chLi <565353780@qq.com>
2024-12-17 13:45:54 +0000

    auto check and skip copied files

--- unzip_glbs.py
+++ unzip_glbs.py
@@ -1,12 +1,11 @@
 import os
 
-def unzipGlbs(glbs_zip_folder_path: str,
-              objaverse_glbs_folder_path: str
-              ) -> bool:
+
+def unzipGlbs(glbs_zip_folder_path: str, objaverse_glbs_folder_path: str) -> bool:
     if not os.path.exists(glbs_zip_folder_path):
-        print('[ERROR][unzip_glbs::unzipGlbs]')
-        print('\t glbs zip folder not exist!')
-        print('\t glbs_zip_folder_path:', glbs_zip_folder_path)
+        print("[ERROR][unzip_glbs::unzipGlbs]")
+        print("\t glbs zip folder not exist!")
+        print("\t glbs_zip_folder_path:", glbs_zip_folder_path)
 
         return False
 
@@ -14,28 +13,31 @@ def unzipGlbs(glbs_zip_folder_path: str,
     glbs_zip_filename_list.sort()
 
     for glbs_zip_filename in glbs_zip_filename_list:
-        if glbs_zip_filename[:4] != '000-' or glbs_zip_filename[-4:] != '.zip':
+        if glbs_zip_filename[:4] != "000-" or glbs_zip_filename[-4:] != ".zip":
             continue
 
+        """
         zip_id = int(glbs_zip_filename[4:7])
 
-        if zip_id >= 160:
+        if not 140 <= zip_id <= 141:
             continue
+        """
 
         glbs_zip_file_path = glbs_zip_folder_path + glbs_zip_filename
 
-        print('[INFO][unzip_glbs::unzipGlbs]')
-        print('\t start unzip file:', glbs_zip_filename, '...')
-        command = 'unzip -u ' + glbs_zip_file_path + ' -d ' + objaverse_glbs_folder_path
+        print("[INFO][unzip_glbs::unzipGlbs]")
+        print("\t start unzip file:", glbs_zip_filename, "...")
+        command = "unzip -u " + glbs_zip_file_path + " -d " + objaverse_glbs_folder_path
 
         os.system(command)
 
     return True
 
+
 if __name__ == "__main__":
     root_list = [
-        '/mnt/data/jintian/chLi/Dataset/',
-        os.environ['HOME'] + '/chLi/Dataset/',
+        "/mnt/d/chLi/Dataset/",
+        os.environ["HOME"] + "/chLi/Dataset/",
     ]
 
     root_folder_path = None
@@ -44,11 +46,11 @@ if __name__ == "__main__":
             root_folder_path = root
             break
     if root_folder_path is None:
-        print('[ERROR][unzip_glbs::__main__]')
-        print('\t dataset not found!')
+        print("[ERROR][unzip_glbs::__main__]")
+        print("\t dataset not found!")
         exit()
 
-    glbs_zip_folder_path = os.environ['HOME'] + '/Downloads/finished/'
-    objaverse_glbs_folder_path = root_folder_path + 'Objaverse_82K/glbs/'
+    glbs_zip_folder_path = "/mnt/d/chLi/finished/"
+    objaverse_glbs_folder_path = root_folder_path + "Objaverse_82K/glbs/"
 
     unzipGlbs(glbs_zip_folder_path, objaverse_glbs_folder_path)
